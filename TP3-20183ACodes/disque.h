3f9089141327933e722110f5082e0b264c26ef05
Debonnaire Ngatchui <debonnairengatchui@gmail.com>
2020-05-06 18:50:38 -0400

    debut backend

--- TP3-20183ACodes/disque.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISQUE_H
-#define DISQUE_H
- 
-// Les fonctions d'acces au disque dur. Les fonctions bd_*
-// utilisent ces fonctions pour lire & ecrire sur le disque.
-int ReadBlock(UINT16 BlockNum, char *pBuffer);
-int WriteBlock(UINT16 BlockNum, const char *pBuffer);
-//20183ASE
-#endif
