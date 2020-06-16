3f02967ca6c8c1b6668d67386d6b8469b53d14f5
Debonnaire Ngatchui <debonnairengatchui@gmail.com>
2020-06-16 12:23:39 -0400

    user.ts

---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    telephone: string;
+    dateInscription: string;
+    password: string;
+  }
