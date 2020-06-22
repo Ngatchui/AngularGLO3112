29acf0ae1d85f992a0903e2a3bf00e22df0354e0
Debonnaire Ngatchui <debonnairengatchui@gmail.com>
2020-06-22 18:06:08 -0400

    composant register et login commencés

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { UserListeComponent } from './components/user-liste/user-liste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    UpdateUserComponent,
+    UserListeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  user: User = new User();
+  submitted = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+  }
+
+}
